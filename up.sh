56016cdc5c970d1c9a446161d4dd2134dd7efc85
rfc2119 <rfc2119@users.noreply.github.com>
2021-07-20 16:51:58 +0000

    parametrize the up.sh script

--- up.sh
+++ up.sh
@@ -2,13 +2,63 @@
 
 set -e
 
+function print_help {
+  echo "usage: $0 [options] <key_pair_name>"
+  echo "runs the eks.yml cloudformation template file and configures"
+  echo "kubectl to talk to the control plane"
+  echo "-h, --help  prints this message"
+  echo "--num-worker-nodes  how many worker nodes to be deployed across two AZs"
+  echo "--stack-name  cloudformation stack name"
+  echo "--worker-nodes-instance-type  aws instancy type of the worker nodes"
+  echo "--key-pair-name name of the private key pair on your aws account"
+  # echo "--update-kubectl-context updates context for installed kubectl"
+}
+
 # EDIT THIS:
 #------------------------------------------------------------------------------#
 NUM_WORKER_NODES=3
 WORKER_NODES_INSTANCE_TYPE=t2.micro
 STACK_NAME=test-cluster
-KEY_PAIR_NAME=qs-us-east-1
+KEY_PAIR_NAME=
 #------------------------------------------------------------------------------#
+
+while [[ $# -gt 0 ]]; do
+    key="$1"
+    case $key in
+        -h|--help)
+            print_help
+            exit 1
+            ;;
+        --num-worker-nodes)
+            NUM_WORKER_NODES="$2"
+            shift
+            shift
+            ;;
+        --worker-nodes-instance-type)
+            WORKER_NODES_INSTANCE_TYPE="$2"
+            shift
+            shift
+            ;;
+        --stack-name)
+            STACK_NAME="$2"
+            shift
+            shift
+            ;;
+        --key-pair-name)
+            KEY_PAIR_NAME="$2"
+            shift
+            shift
+            ;;
+        # --update-kubectl-context)
+        #     UPDATE_CONTEXT=true
+        #     shift
+        #     ;;
+        *)    # unknown option
+            echo "Unknown option: $1"
+            exit 1
+            ;;
+    esac
+done
 
 # Output colours
 COL='\033[1;34m'
@@ -25,6 +75,8 @@ aws cloudformation deploy \
       NumWorkerNodes="$NUM_WORKER_NODES" \
       WorkerNodesInstanceType="$WORKER_NODES_INSTANCE_TYPE"
 
+# by default, the resulting configuration file is created
+# at the default kubeconfig path (.kube/config) in your home directory
 echo -e "\n$COL> Updating kubeconfig file...$NOC"
 aws eks update-kubeconfig "$@" --name "$STACK_NAME" 
 
